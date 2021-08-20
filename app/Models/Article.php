3184632908f3c774a4fc7efa6c4bf79ed733d929
malek <malekhaddar8@gmail.com>
2021-08-20 23:42:59 +0100

    add scope & override bread views

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+
 
 
 class Article extends Model
@@ -15,25 +17,32 @@ class Article extends Model
 
 
 
+    public function scopeCurrentUser($query)
+    {
+        return Auth::user()->hasRole('admin') ? $query : $query->where('owner_id', Auth::user()->id);
+    }
+
     public function save(array $options = [])
     {
-        // If no owner has been assigned,
-        // assign the current user's id as the owner of the workstation
+        // If no owner has been assigned, assign the current user's id as the owner of the workstation
         if (!$this->owner_id && Auth::user()) {
             $this->owner_id = Auth::user()->getKey();
         }
 
         return parent::save();
     }
-}
+    /* public function getRouteKeyName()
+    {
+        return $this->getKeyName();
+    } */
 
-/* public static function boot()
+
+    public static function boot()
     {
         parent::boot();
 
         static::saving(function ($model) {
-            $model->slug = str::slug($model->title);
-
-            return true;
+            $model->slug = Str::slug($model->title);
         });
-    } */
+    }
+}
